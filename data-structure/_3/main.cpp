25fe3040c6aa40e6323ff6abe8891575f5807e03
xihe <citype124@gmail.com>
2020-09-11 22:45:55 +0800

    xcode :浙大 数据结构起步测试 自测题 1-4 finish

---
+++ data-structure/_3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  _3
+//
+//  Created by 西河 on 2020/9/11.
+//  Copyright © 2020 xihe. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ src/_1.java
@@ -0,0 +1,9 @@
+/**
+ * 最大子列和问题：是本次课最后讲到的4种算法的实验题，属于基本要求，一定要做；
+ */
+public class _1 {
+
+    public static void main(String[] args) {
+        int [] arr = {-2,1,23,3};
+    }
+}

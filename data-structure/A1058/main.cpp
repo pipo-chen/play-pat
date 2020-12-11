21e7dd459a17edd106e903bd5734817d60780258
mk2019 <unicorn.mark2019@gmail.com>
2020-12-11 21:39:00 +0800

    1058 A+B in Hogwarts (20point(s)) update

--- data-structure/A1058/main.cpp
+++ data-structure/A1058/main.cpp
@@ -16,31 +16,13 @@ int main(int argc, const char * argv[]) {
     int a[3];
     int b[3];
     scanf("%d.%d.%d %d.%d.%d",&a[0],&a[1],&a[2],&b[0],&b[1],&b[2]);
-    int res[3];
     int carry = 0;
-    for (int i = 2; i >= 0; i--) {
-        int ans = a[i] + b[i] + carry;
-        carry = 0;
-        
-        if (i == 1 && ans >= GALLEON) {
-            carry = ans / GALLEON;
-            ans = ans % GALLEON;
-        }
-        if (i == 2 && ans >= SICKLE) {
+    int v1 = (a[2] + b[2] + carry) % SICKLE;
+    carry = (a[2] + b[2] + carry) / SICKLE;
+    int v2 = (a[1] + b[1] + carry) % GALLEON;
+    carry = (a[1] + b[1] + carry) / GALLEON;
+    int v3 = a[0] + b[0] + carry;
     
-            carry = ans / SICKLE;
-            ans = ans % SICKLE;
-        }
-        //carry 加完之后 没有被重置
-        res[i] = ans;
-    }
-    
-    for (int j = 0; j < 3; j++) {
-        printf("%d",res[j]);
-        if (j != 2) {
-            printf(".");
-        }
-    }
-    printf("\n");
+    printf("%d.%d.%d\n",v3,v2,v1);
     return 0;
 }

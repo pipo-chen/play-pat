d6c6a30efe5a4b2508de72f66799b6d0b5f1732f
mk2019 <unicorn.mark2019@gmail.com>
2021-02-05 15:25:06 +0800

    1005 继续(3n+1)猜想 (25 point(s)) finish

--- data-structure/1005/main.cpp
+++ data-structure/1005/main.cpp
@@ -13,13 +13,51 @@ using namespace ::std;
 bool cmp(int a, int b) {
     return a > b;
 }
+/*
+ //对：只要首个待处理数字出现在 hash_table 的 算是被覆盖的？
+ //还是：只要处理过一次之后 出现在 hash_table 的都是被覆盖的
+ //例如：n=3， 5 8 4 2 1 n=7， 11 17 26 13 20 10 5 8 4 2 1 结果算是 7 3 还是 7？
+int main() {
+    int n, m, a[110];
+    scanf("%d",&n);
+    bool hash_table[10000] = {0};
+    
+    for (int i = 0; i < n; i++) {
+        scanf("%d",&a[i]);
+        m = a[i];
+        while (m != 1) {
+            if (m % 2 == 1)
+                m = (3 * m + 1)/2;
+            else
+                m = m / 2;
+            hash_table[m] = true;
+        }
+    }
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (hash_table[a[i]] == false) {
+            count++;
+        }
+    }
+    sort(a, a+n, cmp);
+    for (int i = 0; i < n; i++) {
+        if (hash_table[a[i]] == false) {
+            printf("%d",a[i]);
+            count--;
+            if (count > 0)
+                printf(" ");
+        }
+    }
+    return 0;
+}
+*/
 //判断有没有被其他数字覆盖
 int main(int argc, const char * argv[]) {
     int K;
     scanf("%d",&K);
     int data[K];
     int res[K];
-    int hash_table[101] = {0};
+    bool hash_table[10000] = { false };
     for (int i = 0; i < K; i++) {
         scanf("%d", &data[i]);
         int digit = data[i];
@@ -31,28 +69,15 @@ int main(int argc, const char * argv[]) {
                 digit = (digit * 3 + 1)/2;
             }
             //记录每一次 digit 的值
-            hash_table[digit]++;
+            hash_table[digit] = true;
         }
     }
+   
     int index = 0;
     for (int i = 0; i < K; i++) {
-        int digit = data[i];
-        while (digit != 1) {
-            if (digit % 2 == 0) {
-                digit /= 2;
-            } else {
-                digit = (digit * 3 + 1)/2;
-            }
-            if (hash_table[digit] > 1) {
-                //说明第一个数值就已经被覆盖了
-                break;
-            } else {
-                //说明第一个数值没有被覆盖
-                res[index++] = data[i];
-                break;
-            }
+        if (!hash_table[data[i]]) {
+            res[index++] = data[i];
         }
-        
     }
     //结果集按从大到小的顺序排列
     sort(res, res + index, cmp);

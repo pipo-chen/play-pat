b4e7f3eeb5901050af85f5bc002168773fbd9c59
mk2019 <unicorn.mark2019@gmail.com>
2021-03-03 21:13:17 +0800

    1093 字符串A+B (20 point(s)) finish by while

--- data-structure/1093/main.cpp
+++ data-structure/1093/main.cpp
@@ -4,7 +4,7 @@
 //
 //  Created by mark on 2021/3/3.
 //  Copyright © 2021 xihe. All rights reserved.
-//
+//  ⚠️发现同样的代码 用 while 会快一些？？？
 
 #include <iostream>
 #include <cstring>
@@ -17,20 +17,22 @@ int main(int argc, const char * argv[]) {
     cin.getline(A, 1000010);
     cin.getline(B, 1000010);
     
-    for (int i = 0; i < strlen(A); i++) {
-        
+    int i = 0;
+    while (A[i]) {
         if (judge[A[i]] == 0) {
             printf("%c",A[i]);
         }
         judge[A[i]] = 1;
+        i++;
     }
-    for (int i = 0; i < strlen(B); i++) {
+    i = 0;
+    while (B[i]) {
         if (judge[B[i]] == 0) {
             printf("%c",B[i]);
         }
         judge[B[i]] = 1;
+        i++;
     }
-    
     printf("\n");
     return 0;
 }

688b25dbe48f3e1ab9de64d23b3aefad3b1fe665
ZH.Chen <unicron.mark2019@gmail.com>
2021-12-06 19:50:31 +0800

    finish L1-004 计算摄氏温度 (5 分)

---
+++ data-structure/L1-004/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  L1-004
+//
+//  Created by zihan on 2021/12/6.
+//  Copyright © 2021 xihe. All rights reserved.
+//
+
+#include <iostream>
+using namespace :: std;
+int main(int argc, const char * argv[]) {
+    int f;
+    scanf("%d",&f);
+    printf("Celsius = %d\n",5 * (f - 32)/9);
+ 
+    return 0;
+}
